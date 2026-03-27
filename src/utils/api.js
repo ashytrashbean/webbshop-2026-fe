3399919a5291d8e9bbd590c6547038342e61f3f6
Annalu Jansson <73094186+ashytrashbean@users.noreply.github.com>
2026-03-27 14:12:00 +0100

    Update getBaseUrl to return new production URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 
 export function getBaseUrl() {
+  return "https://webbshop-2026-be-grupp-2.vercel.app/";
   if (window.location.hostname.includes("localhost")) {
     return "http://localhost:3000/";
   }
   // TODO: Add deployed backend URL
-  return "https://your-backend.vercel.app/";
 }
