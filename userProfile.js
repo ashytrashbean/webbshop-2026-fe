bb2af1893ba0b1dfb0b178bb9c95de1aa315f10d
ashytrashbean <annalu.jansson@gmail.com>
2026-04-20 13:09:27 +0200

    profile has now the users plants and info about their plants and swappes

--- userProfile.js
+++ userProfile.js
@@ -1,23 +1,55 @@
 import { getRegionFromCoords } from "./src/utils/api.js";
+import { getBaseUrl } from "./src/utils/api.js";
 
+function renderPlantCards(plants){
+    const container = document.querySelector("#plant-grid");
+    container.innerHTML = "";
+    let allCardsHtml = "";
+
+    plants.forEach(plant => {
+        allCardsHtml = `
+        <div class="plant-card" data-id="${plant._id}">
+            <img src="${plant.image}" alt="${plant.name}" height="91px">
+            <h4>${plant.name}</h4>
+        </div>
+        `;
+        container.innerHTML += allCardsHtml;
+    });
+}
 
 async function loadProfile() {
     const userString = localStorage.getItem("userInfo");
 
-    if(userString){
-        const user = JSON.parse(userString);
-        document.querySelector(".name").textContent = user.name;
+    const user = JSON.parse(userString);
 
-        if(user.location && user.location.length === 2){
-            const [lat, lon] = user.location;
+    
+    document.querySelector(".name").textContent = user.name;
+    
+    if(user.location && user.location.length === 2){
+        const [lat, lon] = user.location;
+        
+        const regionElement = document.querySelector(".region");
+        regionElement.textContent = "Getting region...";
+        
+        const regionName = await getRegionFromCoords(lat, lon);
+        regionElement.textContent = regionName;
+    }
 
-            const regionElement = document.querySelector(".region");
-            regionElement.textContent = "Getting region...";
+    const url = `${getBaseUrl()}users/id/${user._id}`;
+    const response = await fetch(url)
 
-            const regionName = await getRegionFromCoords(lat, lon);
-            regionElement.textContent = regionName;
-        }
-    } 
+    const userProfile = await response.json();
+
+    // Calculate stats from the fetched userProfile
+    const listedCount = userProfile.plants.length;
+    const pendingCount = userProfile.activeTrades.filter(t => t.status === "pending").length;
+    const swappedCount = userProfile.activeTrades.filter(t => t.status === "approved").length;
+
+    document.querySelector("#listed-number").textContent = listedCount;
+    document.querySelector("#pending-number").textContent = pendingCount;
+    document.querySelector("#swapped-number").textContent = swappedCount;
+    
+    renderPlantCards(userProfile.plants);
 }
 
 loadProfile();
